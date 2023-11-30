d6b51e21febafe30092bce0ad70d4a7cc020ab5b
anderudp <pappandrasdavid@gmail.com>
2023-11-30 12:50:53 +0100

    Added selectbox

--- pima.py
+++ pima.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
 
 
 api = KaggleApi()
 api.authenticate()
 api.dataset_download_files('uciml/pima-indians-diabetes-database', unzip=True)
 
+df = pd.read_csv('diabetes.csv')
+
 st.title("Pima indians diabetes regression")
+st.selectbox("What field should we predict for?", df.columns.sort_values())
