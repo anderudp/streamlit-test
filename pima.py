4fa16745b45ef01d039ec12f1c294332eeaa2c97
anderudp <pappandrasdavid@gmail.com>
2023-12-21 10:25:27 +0100

    afgfhds

--- pima.py
+++ pima.py
@@ -43,15 +43,27 @@ if __name__ == "__main__":
     # Get the best parameters from the grid search
     best_params = grid_search.best_params_
 
-    # Print the best parameters
-    print("Best Parameters:", best_params)
-
     # Use the best parameters to create the final logistic regression model
     final_model = LogisticRegression(C=best_params['C'], penalty=best_params['penalty'], max_iter=1000)
 
     # Train the final model on the entire training set
     final_model.fit(X_train, y_train)
 
-    st.title("DPF regresszió")
-    st.selectbox("What field should we predict for?", df.columns.sort_values())
+    # Frontend
+    st.title("Cukorbetegség predikció")
+    pregnancies = st.number_input("Terhességek száma (#)", value=round(df['Pregnancies'].mean()), min_value=0)
+    glucose = st.number_input("OGTT-2 vérglükóz (mg/dL)", value=round(df['Glucose'].mean()), min_value=0)
+    blood_pressure = st.number_input("Diasztolés vérnyomás (mmHg)", value=round(df['BloodPressure'].mean()), min_value=0)
+    skin_thickness = st.number_input("Tricepsz bőrredővastagsága (mm)", value=round(df['SkinThickness'].mean()), min_value=0)
+    insulin = st.number_input("Biokatív vérinzulin (μU/mL)", value=round(df['Insulin'].mean()), min_value=0)
+    bmi = st.number_input("Testtömegindex (kg/m)", value=round(df['BMI'].mean(), 1), min_value=0.0)
+    dpf = st.number_input("Származás-alapú diabéteszfüggvény (DPF)", value=round(df['DPF'].mean(), 3), min_value=0.0)
+    age = st.number_input("Életkor (év)", value=round(df['Age'].mean()), min_value=0)
+    result = None
+    def btn_callback():
+        result = final_model.predict(np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]]))
+        result_text = "Várhatóan pozitív" if result[0] == 1 else "Várhatóan negatív"
+        st.title(f"Eredmény: {result_text}")
+
+    st.button("Predikció indítása", on_click=btn_callback)
     
