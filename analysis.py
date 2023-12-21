b957b8130da535bf3f5b36c23f1d3577f0c10273
anderudp <pappandrasdavid@gmail.com>
2023-12-21 07:56:48 +0100

    killme

--- analysis.py
+++ analysis.py
@@ -9,14 +9,16 @@ def invalids_pie_chart():
     df["invalids"] = df.isin([0, 0.0, np.nan]).sum(axis=1)
     counts = df.value_counts(["invalids"]).to_frame().reset_index().sort_values("invalids")
 
+    #plt.bar_label("asd")
     fig, ax = plt.subplots()
     ax.pie(
         counts["count"], 
         labels=counts["invalids"],
         colors=["#54ff00", "#e4ff00", "#edaa00", "#f65500", "#870000"],
-        explode=(0, 0, 0, 0.2, 0.2)
+        explode=(0, 0, 0, 0.2, 0.2),
+        autopct='%1.1f%%'
         )
-
+    ax.set_title("Hiányzó adattagok száma a rekordokban")
     plt.savefig("counts.png")
     
     
@@ -26,10 +28,87 @@ def invalid_records_bar_chart():
     records = df.isin([0, 0.0, np.nan]).sum(axis=0).to_frame().reset_index().rename({0:"invalids"}, axis=1)
     records["valids"] = df.shape[0] - records["invalids"]
     
+    plt.title("Érvényes adatok aránya")
     plt.bar(records["index"], records["invalids"], color="#cc0000")
     plt.bar(records["index"], records["valids"], bottom=records["invalids"], color="#009900")
     plt.savefig("bar.png")
     
+
+def dpf_histogram():
+    df = pd.read_csv("diabetes.csv")
+    fig, ax = plt.subplots()
+    plt.xlabel("DPF")
+    plt.ylabel("Rekordok száma")
+    ax.hist(
+        df['DiabetesPedigreeFunction'],
+        bins=100,
+        histtype='stepfilled',
+    )
+    ax.set_title("DPF eloszlása az adathalmazban")
+    plt.savefig("hist.png")
+    
+def insulin_histogram():
+    df = pd.read_csv("diabetes.csv")
+    df["invalids"] = df[["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]].isin([0, 0.0, np.nan]).sum(axis=1)
+    df.drop(df[df["invalids"] >= 3].index, inplace=True)
+    
+    df.drop(df[df["Insulin"] == 0.0].index, inplace=True)
+    
+    fig, ax = plt.subplots()
+    plt.xlabel("Inzulin")
+    plt.ylabel("Rekordok száma")
+    ax.hist(
+        df['Insulin'],
+        bins=100,
+        histtype='stepfilled',
+    )
+    ax.axvline(df['Insulin'].mean(), color='r', linestyle='dashed', linewidth=2)
+    ax.set_title("Inzulin eloszlása az adathalmazban")
+    plt.savefig("insulin_hist.png")
+    
+def features_histogram():
+    df = pd.read_csv("diabetes.csv")
+    df = df.iloc[:, :-1]
+    for col in df.columns:
+        fig, ax = plt.subplots()
+        plt.xlabel(col)
+        plt.ylabel("Rekordok száma")
+        ax.hist(
+            df[col],
+            bins=80,
+            histtype='stepfilled',
+        )
+        ax.axvline(df[col].mean(), color='r', linestyle='dashed', linewidth=2)
+        ax.set_title(f"{col} oszlop eloszlása")
+        plt.savefig(f"features/{col}_hist.png")
+    
+    
+def skin_histogram():
+    df = pd.read_csv("diabetes.csv")
+    df["invalids"] = df[["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]].isin([0, 0.0, np.nan]).sum(axis=1)
+    df.drop(df[df["invalids"] >= 3].index, inplace=True)
+    
+    df.drop(df[df["SkinThickness"] == 0.0].index, inplace=True)
+    
+    fig, ax = plt.subplots()
+    plt.xlabel("Bőrredővastagság")
+    plt.ylabel("Rekordok száma")
+    ax.hist(
+        df['SkinThickness'],
+        bins=80,
+        histtype='stepfilled',
+    )
+    ax.axvline(df['SkinThickness'].mean(), color='r', linestyle='dashed', linewidth=2)
+    ax.set_title("Bőrredővastagság eloszlása az adathalmazban")
+    plt.savefig("skin_hist.png")
+    
+def outcome_bar():
+    df = pd.read_csv("diabetes.csv")
+    plt.title("Végkimenetek eloszlása")
+    plt.bar(['Negatív', 'Pozitív'], df['Outcome'].value_counts().sort_index())
+    plt.savefig('outcome_bar.png')
+    
+    print(df['Outcome'].value_counts().sort_index())
     
 if __name__ == "__main__":
-    invalid_records_bar_chart()
+    outcome_bar()
--- pima.py
+++ pima.py
@@ -1,14 +1,62 @@
 import streamlit as st
 from kaggle.api.kaggle_api_extended import KaggleApi
 import pandas as pd
+import numpy as np
+from scipy.stats import uniform
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, classification_report, confusion_matrix, make_scorer
+
 
 if __name__ == "__main__":
-    api = KaggleApi()
-    api.authenticate()
-    api.dataset_download_files('uciml/pima-indians-diabetes-database', unzip=True)
-
+    # Getting the dataset
+    # api = KaggleApi()
+    # api.authenticate()
+    # api.dataset_download_files('uciml/pima-indians-diabetes-database', unzip=True)
     df = pd.read_csv('diabetes.csv')
+    
+    # Quick rename
+    df.rename(columns={"DiabetesPedigreeFunction":"DPF"}, inplace=True)
+    
+    # Drop 3, 4 invalid records
+    df["invalids"] = df[["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]].isin([0, 0.0, np.nan]).sum(axis=1)
+    df.drop(df[df["invalids"] >= 3].index, inplace=True)
+    df.drop(["invalids"], axis=1, inplace=True)
+    
+    # Replace zeroes with column avg on affected datapoints
+    df['Glucose'].replace(0, np.NaN, inplace=True)
+    df['BloodPressure'].replace(0, np.NaN, inplace=True)
+    df['SkinThickness'].replace(0, np.NaN, inplace=True)
+    df['Insulin'].replace(0, np.NaN, inplace=True)
+    df['BMI'].replace(0, np.NaN, inplace=True)
+    df.fillna(df.mean(), inplace=True)
+    
+    # Split that bih
+    X = df.drop('Outcome', axis=1)
+    y = df['Outcome']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-    st.title("Pima indians diabetes regression")
+    # Logreg + optimize
+    model = LogisticRegression(solver='lbfgs', penalty='l2', max_iter=1000)
+    param_grid = {
+        'C': [0.01],
+        'penalty': ['l2']
+    }
+    grid_search = GridSearchCV(model, param_grid, scoring='recall', cv=100)
+    grid_search.fit(X_train, y_train)
+
+    # Get the best parameters from the grid search
+    best_params = grid_search.best_params_
+
+    # Print the best parameters
+    print("Best Parameters:", best_params)
+
+    # Use the best parameters to create the final logistic regression model
+    final_model = LogisticRegression(C=best_params['C'], penalty=best_params['penalty'], max_iter=1000)
+
+    # Train the final model on the entire training set
+    final_model.fit(X_train, y_train)
+
+    st.title("DPF regresszió")
     st.selectbox("What field should we predict for?", df.columns.sort_values())
     
